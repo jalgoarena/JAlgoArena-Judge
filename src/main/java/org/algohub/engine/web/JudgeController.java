168c255c8ef6d3c0175e4041b8be14ac51339357
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-07-26 15:55:55 +0200

    - move out of shadow jar in favor of spring boot

--- src/main/java/org/algohub/engine/web/JudgeController.java
+++ src/main/java/org/algohub/engine/web/JudgeController.java
@@ -1,7 +1,6 @@
 package org.algohub.engine.web;
 
 import com.google.common.base.Charsets;
-import com.google.common.io.Files;
 import com.google.common.io.Resources;
 import org.algohub.engine.codegenerator.JavaCodeGenerator;
 import org.algohub.engine.judge.JudgeEngine;
@@ -11,23 +10,12 @@ import org.algohub.engine.serde.ObjectMapperInstance;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 
 @Controller
 class JudgeController {
 
     private final transient JudgeEngine judgeEngine = new JudgeEngine();
-
-    private static Problem getProblem(String arg) throws IOException {
-        final String problemStr = asCharSource(arg);
-        return ObjectMapperInstance.INSTANCE.readValue(problemStr, Problem.class);
-    }
-
-    private static String asCharSource(String problemName) throws IOException {
-        return Files.asCharSource(new File(problemName), Charsets.UTF_8).read();
-    }
 
     @RequestMapping(path = "/problems/{id}/solution", method = RequestMethod.POST)
     @ResponseBody
@@ -45,8 +33,7 @@ class JudgeController {
     @RequestMapping("/problems/{id}")
     @ResponseBody
     Problem problem(@PathVariable String id) throws IOException {
-        URL resource = Resources.getResource(id + ".json");
-        return getProblem(resource.getFile()).toPublicProblem();
+        return getProblemById(id).toPublicProblem();
     }
 
     @RequestMapping("/problems/{id}/skeletonCode")
@@ -57,7 +44,10 @@ class JudgeController {
     }
 
     private Problem getProblemById(String id) throws IOException {
-        URL resource = Resources.getResource(id + ".json");
-        return getProblem(resource.getFile());
+        String problemAsJson = Resources.toString(
+                Resources.getResource(id + ".json"), Charsets.UTF_8
+        );
+
+        return ObjectMapperInstance.INSTANCE.readValue(problemAsJson, Problem.class);
     }
 }
