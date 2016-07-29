de8fd236e32b243bcbdc7573bb20b26eab6cbea7
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-07-29 14:27:30 +0200

    - fixes after Sonar run (2)

--- src/main/java/org/algohub/engine/compiler/java/CompileErrorException.java
+++ src/main/java/org/algohub/engine/compiler/java/CompileErrorException.java
@@ -1,6 +1,9 @@
 package org.algohub.engine.compiler.java;
 
-class CompileErrorException extends Exception {
+/**
+ * Compile error exception - raised when source code is run against java compiler
+ */
+public class CompileErrorException extends Exception {
     CompileErrorException(final String message) {
         super(message);
     }
