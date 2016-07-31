65a1529dcf75f203f06507531f1f8ee92acbb7d9
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-07-31 08:57:58 +0200

    - add more unit tests for covering LinkedListNode

--- src/test/java/org/algohub/engine/LinkedListNodeSpec.java
+++ src/test/java/org/algohub/engine/LinkedListNodeSpec.java
@@ -10,14 +10,7 @@ public class LinkedListNodeSpec {
     @Test
     public void stringRepresentationCorrespondentsLinkedListValues() throws Exception {
 
-        LinkedListNode<Integer> node = new LinkedListNode<>(1,
-                new LinkedListNode<>(2,
-                        new LinkedListNode<>(3,
-                                new LinkedListNode<>(4)
-                        )
-                )
-        );
-        assertThat(node.toString()).isEqualTo("[1, 2, 3, 4]");
+        assertThat(linkedList_1_2_3_4().toString()).isEqualTo("[1, 2, 3, 4]");
     }
 
     @Test
@@ -29,7 +22,50 @@ public class LinkedListNodeSpec {
     }
 
     @Test
-    public void defaultConstructorCreatesEmptyNode() throws Exception {
-        assertThat(new LinkedListNode<Integer>().value).isNull();
+    public void defaultConstructorCreatesEmptyLinkedList() throws Exception {
+        assertThat(new LinkedListNode<Integer>().toString()).isEqualTo("[]");
+    }
+
+    @Test
+    @SuppressWarnings("all")
+    public void equalsItself() throws Exception {
+        LinkedListNode<Integer> node = new LinkedListNode<>();
+        assertThat(node.equals(node)).isTrue();
+    }
+
+    @Test
+    public void equalsFalseIfComparedToDifferentObject() throws Exception {
+
+        assertThat(new LinkedListNode<Integer>().equals(new Object())).isFalse();
+    }
+
+    @Test
+    public void equalsFalseIfLinkedListIsDifferentByAnyElement() throws Exception {
+
+        LinkedListNode<Integer> list1 = linkedList_1_2_3_4();
+        list1.add(5);
+        LinkedListNode<Integer> list2 = linkedList_1_2_3_4();
+
+        assertThat(list1.equals(list2)).isFalse();
+    }
+
+    @Test
+    public void hashCodeIsDifferentForDifferentLinkedLists() throws Exception {
+
+        LinkedListNode<Integer> list1 = linkedList_1_2_3_4();
+        list1.add(5);
+        LinkedListNode<Integer> list2 = linkedList_1_2_3_4();
+
+        assertThat(list1.hashCode() != list2.hashCode()).isTrue();
+    }
+
+    private static LinkedListNode<Integer> linkedList_1_2_3_4() {
+        return new LinkedListNode<>(1,
+                new LinkedListNode<>(2,
+                        new LinkedListNode<>(3,
+                                new LinkedListNode<>(4)
+                        )
+                )
+        );
     }
 }
