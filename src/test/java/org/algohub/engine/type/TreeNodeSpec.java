121ff3234f59cdb57c8ce42a2e1d13c9f38fe721
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-11-06 17:52:20 +0100

    add test for tree node equality

--- src/test/java/org/algohub/engine/type/TreeNodeSpec.java
+++ src/test/java/org/algohub/engine/type/TreeNodeSpec.java
@@ -52,6 +52,39 @@ public class TreeNodeSpec {
         assertThat(treeNodeAsString).isEqualTo("{\"data\":1,\"left\":{\"data\":2,\"left\":{\"data\":4},\"right\":{\"data\":5}},\"right\":{\"data\":3,\"left\":{\"data\":6},\"right\":{\"data\":7}}}");
     }
 
+    @Test
+    public void two_trees_with_same_nodes_in_same_order_are_equal() throws Exception {
+        TreeNode root1 = new TreeNode(
+                1,
+                new TreeNode(
+                        2,
+                        new TreeNode(4),
+                        new TreeNode(5)
+                ),
+                new TreeNode(
+                        3,
+                        new TreeNode(6),
+                        new TreeNode(7)
+                )
+        );
+
+        TreeNode root2 = new TreeNode(
+                1,
+                new TreeNode(
+                        2,
+                        new TreeNode(4),
+                        new TreeNode(5)
+                ),
+                new TreeNode(
+                        3,
+                        new TreeNode(6),
+                        new TreeNode(7)
+                )
+        );
+
+        assertThat(root1).isEqualTo(root2);
+    }
+
     private void assertSerializationFor(TreeNode root) throws java.io.IOException {
         String treeNodeAsString = ObjectMapperInstance.INSTANCE.writeValueAsString(root);
         TreeNode deserializedTreeNode = ObjectMapperInstance.INSTANCE.readValue(treeNodeAsString, TreeNode.class);
