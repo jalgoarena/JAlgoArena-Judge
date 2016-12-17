80bed5d4f0e70541f56a97b2c4a5cd23df352d04
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-12-17 19:34:39 +0100

    - add tests for kotlin code generator

---
+++ src/test/kotlin/org/algohub/engine/codegeneration/KotlinCodeGeneratorTest.kt
@@ -0,0 +1,71 @@
+package org.algohub.engine.codegeneration
+
+import org.algohub.engine.judge.Function
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class KotlinCodeGeneratorTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun generates_skeleton_function_for_TWO_SUM() {
+        val twoSumGenerated = KotlinCodeGenerator.generateEmptyFunction(TWO_SUM)
+
+        val twoSumExpected = """import java.util.*
+import org.algohub.engine.type.*
+
+class Solution {
+    /**
+     * @param numbers An array of Integers
+     * @param target target = numbers[index1] + numbers[index2]
+     * @return [index1 + 1, index2 + 1] (index1 < index2)
+     */
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        // Write your code here
+    }
+}
+"""
+        assertThat(twoSumGenerated).isEqualTo(twoSumExpected)
+    }
+
+    @Test
+    fun generates_skeleton_function_for_WORD_LADDER() {
+        val wordLadderGenerated = KotlinCodeGenerator.generateEmptyFunction(WORD_LADDER)
+
+        val wordLadderExpected = """import java.util.*
+import org.algohub.engine.type.*
+
+class Solution {
+    /**
+     * @param begin_word the begin word
+     * @param end_word the end word
+     * @param dict the dictionary
+     * @return The shortest length
+     */
+    fun ladderLength(begin_word: String, end_word: String, dict: HashSet): Int {
+        // Write your code here
+    }
+}
+"""
+        assertThat(wordLadderGenerated).isEqualTo(wordLadderExpected)
+    }
+
+    companion object {
+        private val TWO_SUM = Function("twoSum",
+                Function.Return("[I",
+                        "[index1 + 1, index2 + 1] (index1 < index2)"),
+                arrayOf(Function.Parameter("numbers", "[I", "An array of Integers"),
+                        Function.Parameter("target", "java.lang.Integer",
+                                "target = numbers[index1] + numbers[index2]")
+                )
+        )
+
+        private val WORD_LADDER = Function("ladderLength",
+                Function.Return("java.lang.Integer", "The shortest length"),
+                arrayOf(Function.Parameter("begin_word", "java.lang.String", "the begin word"),
+                        Function.Parameter("end_word", "java.lang.String", "the end word"),
+                        Function.Parameter("dict", "java.util.HashSet", "the dictionary")
+                )
+        )
+    }
+}
