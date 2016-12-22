e38c546e63f2b17ecfeb031135c5900fe628064b
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-12-22 16:58:07 +0100

    - cleanup sonar step from travis build script - remove java plugin usage, as it's kotlin based - remove swagger stuff - as it's not API product it mainly adds overhead - but little value (it can be handled easily with Postman) if needed

--- src/main/kotlin/org/algohub/engine/JudgeController.kt
+++ src/main/kotlin/org/algohub/engine/JudgeController.kt
@@ -1,6 +1,5 @@
 package org.algohub.engine
 
-import io.swagger.annotations.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.algohub.engine.codegeneration.JavaCodeGenerator
@@ -11,27 +10,20 @@ import org.algohub.engine.judge.JudgeResult
 import org.algohub.engine.judge.Problem
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
-import java.io.IOException
 import java.util.*
 import java.util.stream.Collectors
 
+@CrossOrigin
 @RestController
-internal class JudgeController {
+class JudgeController {
 
-    @ApiOperation(value = "judge", nickname = "judge")
-    @ApiImplicitParams(ApiImplicitParam(name = "id", value = "Problem's id", required = true, dataType = "string", paramType = "path", defaultValue = "fib"))
-    @ApiResponses(value = *arrayOf(ApiResponse(code = 200, message = "Success", response = JudgeResult::class), ApiResponse(code = 404, message = "Not Found"), ApiResponse(code = 500, message = "Failure")))
     @RequestMapping(path = arrayOf("/problems/{id}/submit"), method = arrayOf(RequestMethod.POST), produces = arrayOf("application/json"))
-    @CrossOrigin
     fun judge(@PathVariable id: String, @RequestBody sourceCode: String): JudgeResult {
         val problem = requestProblem(id) ?: return JudgeResult.runtimeError("Wrong problem id: " + id)
         return JudgeEngine.judge(problem, sourceCode)
     }
 
-    @ApiOperation(value = "problems", nickname = "getProblems")
-    @ApiResponses(value = *arrayOf(ApiResponse(code = 200, message = "Success", response = Problem::class, responseContainer = "List"), ApiResponse(code = 404, message = "Not Found"), ApiResponse(code = 500, message = "Failure")))
     @RequestMapping(path = arrayOf("/problems"), method = arrayOf(RequestMethod.GET), produces = arrayOf("application/json"))
-    @CrossOrigin
     fun problems(): List<Problem> {
         return Arrays.stream(requestProblems())
                 .map { x -> x.problemWithoutFunctionAndTestCases(
@@ -40,7 +32,6 @@ internal class JudgeController {
                 .collect(Collectors.toList<Problem>())
     }
 
-    @Throws(IOException::class)
     private fun requestProblems(): Array<Problem> {
         val request = Request.Builder()
                 .url(DATA_SERVICE_HOST + "problems")
@@ -52,11 +43,7 @@ internal class JudgeController {
         return ObjectMapperInstance.INSTANCE.readValue(problemsAsJsonArray, Array<Problem>::class.java)
     }
 
-    @ApiOperation(value = "problem", nickname = "getProblem")
-    @ApiImplicitParams(ApiImplicitParam(name = "id", value = "Problem's id", required = true, dataType = "string", paramType = "path", defaultValue = "fib"))
-    @ApiResponses(value = *arrayOf(ApiResponse(code = 200, message = "Success", response = Problem::class), ApiResponse(code = 404, message = "Not Found"), ApiResponse(code = 500, message = "Failure")))
     @RequestMapping(path = arrayOf("/problems/{id}"), method = arrayOf(RequestMethod.GET), produces = arrayOf("application/json"))
-    @CrossOrigin
     fun problem(@PathVariable id: String): Problem {
 
         val problem = requestProblem(id) ?: throw IllegalArgumentException("Invalid problem id: " + id)
