ab7ba559fa7abad47e45a77e0de3362fc2b92eca
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-12-25 15:19:11 +0100

    - rename url to data service after adding api to route

--- src/main/kotlin/com/jalgoarena/data/ProblemsRepository.kt
+++ src/main/kotlin/com/jalgoarena/data/ProblemsRepository.kt
@@ -11,12 +11,13 @@ import org.springframework.stereotype.Repository
 class ProblemsRepository {
 
     private val LOG = LoggerFactory.getLogger(this.javaClass)
-    private val DATA_SERVICE_HOST = "https://jalgoarena-api.herokuapp.com/problems/"
+
+    private val DATA_SERVICE_HOST = "https://jalgoarena-api.herokuapp.com/problems/api"
     private val CLIENT = OkHttpClient()
 
     fun find(problemId: String): Problem? {
         val request = Request.Builder()
-                .url("${DATA_SERVICE_HOST}problems/$problemId")
+                .url("$DATA_SERVICE_HOST/problems/$problemId")
                 .build()
 
         val response = CLIENT.newCall(request).execute()
@@ -32,7 +33,7 @@ class ProblemsRepository {
 
     fun findAll(): Array<Problem> {
         val request = Request.Builder()
-                .url(DATA_SERVICE_HOST + "problems")
+                .url("$DATA_SERVICE_HOST/problems")
                 .build()
 
         val response = CLIENT.newCall(request).execute()
