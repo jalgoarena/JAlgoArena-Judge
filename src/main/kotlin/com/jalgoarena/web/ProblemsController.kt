bef1706c0e2d96e9bc2048e37ff6d9d05e7993cc
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-12-30 10:52:52 +0100

    - simplify logic

--- src/main/kotlin/com/jalgoarena/web/ProblemsController.kt
+++ src/main/kotlin/com/jalgoarena/web/ProblemsController.kt
@@ -6,9 +6,10 @@ import com.jalgoarena.data.ProblemsRepository
 import com.jalgoarena.judge.Function
 import com.jalgoarena.judge.Problem
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.*
-import java.util.*
-import java.util.stream.Collectors
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
 import javax.inject.Inject
 
 @CrossOrigin
@@ -22,17 +23,17 @@ class ProblemsController(
 
     @GetMapping("/problems", produces = arrayOf("application/json"))
     fun problems(): List<Problem> {
-        return Arrays.stream(problemsRepository.findAll())
+        return problemsRepository.findAll().asList()
                 .map { x -> x.problemWithoutFunctionAndTestCases(
                         sourceCodeOf(x.function!!), kotlinSourceCodeOf(x.function)
                 )}
-                .collect(Collectors.toList<Problem>())
     }
 
     @GetMapping("/problems/{id}", produces = arrayOf("application/json"))
     fun problem(@PathVariable id: String): Problem {
 
-        val problem = problemsRepository.find(id) ?: throw IllegalArgumentException("Invalid problem id: " + id)
+        val problem = problemsRepository.find(id) ?:
+                throw IllegalArgumentException("Invalid problem id: " + id)
 
         return problem.problemWithoutFunctionAndTestCases(
                 sourceCodeOf(problem.function!!), kotlinSourceCodeOf(problem.function)
