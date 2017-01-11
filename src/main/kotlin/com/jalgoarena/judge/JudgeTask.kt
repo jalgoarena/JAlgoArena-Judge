cda26c32aa38192dc4833b76f8083c749362982f
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-11 21:29:49 +0100

    - simplify Judge Task - better handling of exceptions

--- src/main/kotlin/com/jalgoarena/judge/JudgeTask.kt
+++ src/main/kotlin/com/jalgoarena/judge/JudgeTask.kt
@@ -2,44 +2,33 @@ package com.jalgoarena.judge
 
 import com.jalgoarena.utils.BetterEquals
 import com.jalgoarena.utils.ThrowableEnhancements
-import org.slf4j.LoggerFactory
-import java.lang.reflect.InvocationTargetException
 import java.lang.reflect.Method
 import java.util.concurrent.Callable
 
-internal class JudgeTask(private val clazz: Any, private val method: Method, private val testCases: Array<InternalTestCase>)
-    : Callable<List<Boolean>>, ThrowableEnhancements, BetterEquals {
+internal class JudgeTask(
+        private val clazz: Any,
+        private val method: Method,
+        private val testCases: Array<InternalTestCase>
+) : Callable<List<Boolean>>, ThrowableEnhancements, BetterEquals {
 
-    private val LOG = LoggerFactory.getLogger(this.javaClass)
+    override fun call() =
+            testCases.map { judge(clazz, method, it) }
 
-    override fun call(): List<Boolean> = testCases.map { judge(clazz, method, it) }
+    private fun judge(clazz: Any, method: Method, testCase: InternalTestCase) = try {
+        invokeAndCheck(clazz, method, testCase)
+    } catch(e: Throwable) {
+        val cause = getCause(e)
 
-    private fun judge(clazz: Any, method: Method, testCase: InternalTestCase): Boolean {
-
-        val output: Any?
-        val input: Array<Any?>
-
-        try {
-            input = testCase.input
-            output = method.invoke(clazz, *input)
-        } catch (e: IllegalAccessException) {
-            val cause = getCause(e)
-            LOG.error("Error during processing of solution", cause)
-            throw InterruptedException(cause.javaClass.name + ": " + cause.message)
-        } catch (e: InvocationTargetException) {
-            val cause = getCause(e)
-
-            if (cause is NullPointerException) {
-                return false
-            }
-
-            LOG.error("Error during processing of solution", cause)
-            throw InterruptedException(cause.javaClass.name + ": " + cause.message)
-        } catch (e: IllegalArgumentException) {
-            val cause = getCause(e)
-            LOG.error("Error during processing of solution", cause)
-            throw InterruptedException(cause.javaClass.name + ": " + cause.message)
+        if (cause is NullPointerException) {
+            false
+        } else {
+            throw InterruptedException("${cause.javaClass.name}: ${cause.message}")
         }
+    }
+
+    private fun invokeAndCheck(clazz: Any, method: Method, testCase: InternalTestCase): Boolean {
+        val input = testCase.input
+        val output = method.invoke(clazz, *input)
 
         return equalForObjectsOrArrays(
                 testCase.output,
