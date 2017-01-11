c7f9a2431dbabe120fa336b36ca52de1985a92a7
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-11 08:10:36 +0100

    - replace OkHttpClient with RestTemplate (couldn't even mock the previous, and now code is simpler) - add unit tests for ProblemsClient class

--- src/main/kotlin/com/jalgoarena/ApplicationConfiguration.kt
+++ src/main/kotlin/com/jalgoarena/ApplicationConfiguration.kt
@@ -6,11 +6,7 @@ import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.jalgoarena.codegeneration.JavaCodeGenerator
 import com.jalgoarena.codegeneration.KotlinCodeGenerator
-import com.jalgoarena.web.ProblemsClient
-import com.jalgoarena.judge.JudgeEngine
 import com.jalgoarena.type.ListNode
-import com.jalgoarena.web.JudgeController
-import com.jalgoarena.web.ProblemsController
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
--- src/main/kotlin/com/jalgoarena/web/ProblemsClient.kt
+++ src/main/kotlin/com/jalgoarena/web/ProblemsClient.kt
@@ -1,19 +1,17 @@
 package com.jalgoarena.web
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.jalgoarena.data.DataRepository
 import com.jalgoarena.domain.Problem
 import com.netflix.discovery.EurekaClient
-import okhttp3.OkHttpClient
-import okhttp3.Request
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestOperations
 import javax.inject.Inject
 
 @Service
 class ProblemsClient(
-        @Inject val objectMapper: ObjectMapper,
-        @Inject val discoveryClient: EurekaClient
+        @Inject private val discoveryClient: EurekaClient,
+        @Inject private val restTemplate: RestOperations
 ) : DataRepository<Problem> {
 
     private val LOG = LoggerFactory.getLogger(this.javaClass)
@@ -21,32 +19,22 @@ class ProblemsClient(
     private fun problemsServiceUrl(): String =
             discoveryClient.getNextServerFromEureka("jalgoarena-problems", false).homePageUrl
 
-    private val CLIENT = OkHttpClient()
-
     override fun find(id: String): Problem? {
-        val request = Request.Builder()
-                .url("${problemsServiceUrl()}/problems/$id")
-                .build()
+        val problem = restTemplate.getForObject(
+                "${problemsServiceUrl()}/problems/$id", Problem::class.java
+        )
 
-        val response = CLIENT.newCall(request).execute()
-        val problemAsJson = response.body().string()
-
-        if (problemAsJson.isEmpty() || problemAsJson.contains("com.netflix.zuul.exception.ZuulException")) {
-            LOG.error("There is error in querying for $id problem. Response: $problemAsJson")
+        if (problem == null) {
+            LOG.error("There is error in querying for $id problem. Response: $problem")
             return null
         }
 
-        return objectMapper.readValue(problemAsJson, Problem::class.java)
+        return problem
     }
 
     override fun findAll(): Array<Problem> {
-        val request = Request.Builder()
-                .url("${problemsServiceUrl()}/problems")
-                .build()
-
-        val response = CLIENT.newCall(request).execute()
-        val problemsAsJsonArray = response.body().string()
-
-        return objectMapper.readValue(problemsAsJsonArray, Array<Problem>::class.java)
+        return restTemplate.getForObject(
+                "${problemsServiceUrl()}/problems", Array<Problem>::class.java
+        )
     }
 }
