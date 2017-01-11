db63ae0e4fe62151579907360d18aa9b40461245
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-11 08:39:39 +0100

    - simplify ProblemsClient code

--- src/main/kotlin/com/jalgoarena/web/ProblemsClient.kt
+++ src/main/kotlin/com/jalgoarena/web/ProblemsClient.kt
@@ -3,7 +3,6 @@ package com.jalgoarena.web
 import com.jalgoarena.data.DataRepository
 import com.jalgoarena.domain.Problem
 import com.netflix.discovery.EurekaClient
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestOperations
 import javax.inject.Inject
@@ -14,27 +13,14 @@ class ProblemsClient(
         @Inject private val restTemplate: RestOperations
 ) : DataRepository<Problem> {
 
-    private val LOG = LoggerFactory.getLogger(this.javaClass)
-
     private fun problemsServiceUrl(): String =
             discoveryClient.getNextServerFromEureka("jalgoarena-problems", false).homePageUrl
 
-    override fun find(id: String): Problem? {
-        val problem = restTemplate.getForObject(
-                "${problemsServiceUrl()}/problems/$id", Problem::class.java
-        )
+    override fun find(id: String): Problem? = restTemplate.getForObject(
+            "${problemsServiceUrl()}/problems/$id", Problem::class.java
+    )
 
-        if (problem == null) {
-            LOG.error("There is error in querying for $id problem. Response: $problem")
-            return null
-        }
-
-        return problem
-    }
-
-    override fun findAll(): Array<Problem> {
-        return restTemplate.getForObject(
-                "${problemsServiceUrl()}/problems", Array<Problem>::class.java
-        )
-    }
+    override fun findAll(): Array<Problem> = restTemplate.getForObject(
+            "${problemsServiceUrl()}/problems", Array<Problem>::class.java
+    )
 }
