3e8bc4c9066e62fa2eaec0cfd507630d5e29a93f
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-12 07:05:27 +0100

    - add integration test for checking correctness of spring cofniguration - add codecov configuration with ignoring JudgeApplication from coverage (fun main)

---
+++ src/test/kotlin/com/jalgoarena/JAlgoArenaJudgeAppIntegrationTest.kt
@@ -0,0 +1,29 @@
+package com.jalgoarena
+
+import com.netflix.discovery.EurekaClient
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.junit4.SpringRunner
+import javax.inject.Inject
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class JAlgoArenaJudgeAppIntegrationTest {
+
+    @Inject
+    private lateinit var restTemplate: TestRestTemplate
+
+    @MockBean
+    private lateinit var eurekaClient: EurekaClient
+
+    @Test
+    fun check_if_spring_configuration_works_properly() {
+        val body = this.restTemplate.getForObject("/info", String::class.java)
+        assertThat(body).isEqualTo("{}")
+    }
+}
