49e5d214f8ef9e049ca74c473b936cba5a8bcd26
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-25 08:06:24 +0100

    - new feature - make problems service result cachable

--- src/main/kotlin/com/jalgoarena/ApplicationConfiguration.kt
+++ src/main/kotlin/com/jalgoarena/ApplicationConfiguration.kt
@@ -7,11 +7,18 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.jalgoarena.codegeneration.JavaCodeGenerator
 import com.jalgoarena.codegeneration.KotlinCodeGenerator
 import com.jalgoarena.type.ListNode
+import org.slf4j.LoggerFactory
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
+import org.springframework.cache.CacheManager
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+
 
 @Configuration
+@EnableCaching
 open class ApplicationConfiguration {
 
     @Bean
@@ -33,4 +40,21 @@ open class ApplicationConfiguration {
 
     @Bean
     open fun javaCodeGenerator() = JavaCodeGenerator()
+
+    @Component
+    open class CacheManagerCheck(private val cacheManager: CacheManager) : CommandLineRunner {
+
+        private val LOG = LoggerFactory.getLogger(this.javaClass)
+
+        override fun run(vararg strings: String) {
+            LOG.info("""
+
+=========================================================
+Using cache manager: ${this.cacheManager.javaClass.name}
+=========================================================
+
+"""
+            )
+        }
+    }
 }
--- src/main/kotlin/com/jalgoarena/web/ProblemsClient.kt
+++ src/main/kotlin/com/jalgoarena/web/ProblemsClient.kt
@@ -3,12 +3,13 @@ package com.jalgoarena.web
 import com.jalgoarena.data.DataRepository
 import com.jalgoarena.domain.Problem
 import com.netflix.discovery.EurekaClient
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestOperations
 import javax.inject.Inject
 
 @Service
-class ProblemsClient(
+open class ProblemsClient(
         @Inject private val discoveryClient: EurekaClient,
         @Inject private val restTemplate: RestOperations
 ) : DataRepository<Problem> {
@@ -16,10 +17,12 @@ class ProblemsClient(
     private fun problemsServiceUrl(): String =
             discoveryClient.getNextServerFromEureka("jalgoarena-problems", false).homePageUrl
 
+    @Cacheable("problem")
     override fun find(id: String): Problem = restTemplate.getForObject(
             "${problemsServiceUrl()}/problems/$id", Problem::class.java
     )
 
+    @Cacheable("problems")
     override fun findAll(): Array<Problem> = restTemplate.getForObject(
             "${problemsServiceUrl()}/problems", Array<Problem>::class.java
     )
