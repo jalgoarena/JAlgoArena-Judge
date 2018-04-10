3b4cd96400c74311895546ffc9e97476b52ea4db
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-04-10 22:21:49 +0200

    - update travis config

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-EUREKA_URL=http://localhost:5000/eureka/ PORT=8080 nohup java -Xms1g -Xmx2g -XX:+HeapDumpOnOutOfMemoryError -XX:OnOutOfMemoryError="kill -9 %p;./run.sh" -classpath "lib/*" -jar jalgoarena-judge-*.jar >/dev/null 2>&1 &
+EUREKA_URL=http://localhost:5000/eureka/ nohup java -Xms1g -Xmx2g -XX:+HeapDumpOnOutOfMemoryError -XX:OnOutOfMemoryError="kill -9 %p;./run.sh" -classpath "lib/*" -jar jalgoarena-judge-*.jar >/dev/null 2>&1 &
